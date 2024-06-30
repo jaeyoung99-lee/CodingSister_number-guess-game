558117c287aebc73f46a09f8a49366041b0937ee
jy01.lee <jy01.lee@smarterbuild.net>
2024-07-01 02:18:51 +0900

    second commit

--- main.js
+++ main.js
@@ -12,8 +12,17 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 10;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function(){
+    userInput.value = "";
+})
 
 function pickRandomNum(){
     computerNum = Math.floor(Math.random() * 100) + 1;
@@ -22,13 +31,48 @@ function pickRandomNum(){
 
 function play(){
     let userValue = userInput.value;
+
+    if(userValue < 1 || userValue > 100){
+        resultArea.textContent = "1 ~ 100 사이 숫자를 입력해 주세요!";
+        return 0;
+    }
+
+    if(history.includes(userValue)){
+        resultArea.textContent = "이미 입력한 숫자입니다! 다른 숫자를 입력해 주세요!";
+        return 0;
+    }
+
+    chances--;
+    chanceArea.textContent = `남은 기회 : ${chances}번`;
+
     if(userValue < computerNum){
         resultArea.textContent = "UP!!!";
     }else if(userValue > computerNum){
         resultArea.textContent = "DOWN!!!";
     }else{
         resultArea.textContent = "맞추셨습니다!!!";
+        gameOver = true;
     }
+
+    history.push(userValue);
+
+    if(chances < 1){
+        gameOver = true;
+    }
+
+    if(gameOver == true){
+        playButton.disabled = true;
+    }
+}
+
+function reset(){
+    // user input 창이 깨끗하게 정리
+    userInput.value = "";
+
+    // 새로운 번호가 생성
+    pickRandomNum();
+
+    resultArea.textContent = "결과값이 여기 나옵니다!";
 }
 
 pickRandomNum();
